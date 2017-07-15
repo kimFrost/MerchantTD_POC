215ba6bfe0494a1b52db049e0da0b424b887377f
kimFrost <kim@thatworks.dk>
2017-07-15 22:45:30 +0200

    ()

---
+++ Source/MerchantTD_POC/DataHolder.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DataHolder.generated.h"
+
+
+
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class MERCHANTTD_POC_API UDataHolder : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
--- Source/MerchantTD_POC/MerchantTD_POC.h
+++ Source/MerchantTD_POC/MerchantTD_POC.h
@@ -3,4 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 
+
+/*** FST_Collection ***/
+
+USTRUCT(BlueprintType)
+struct FST_Collection : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FST_Collection(
+		FString Id = "",
+		FString Title = "",
+		TArray<FString> Resources = TArray<FString>(),
+		int PetalsRequired = 5)
+		: Id(Id)
+		, Title(Title)
+		, Resources(Resources)
+		, PetalsRequired(PetalsRequired)
+	{}
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+	FString Id;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+	FString Title;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+	TArray<FString> Resources;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+	int PetalsRequired;
+	/*
+	void SetInt(const int32 NewValue)
+	{
+		SampleInt32 = NewValue;
+	}
+	*/
+};
